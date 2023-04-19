bf69ba53c9839bc1be2c56f3525a4fb40441af09
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-04-19 19:14:38 -0300

    commit

---
+++ dashboard.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+# import ploty.express as px
+# import plotly.graph_objects as go
+
+
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+st.header('Bem vindo à minha Dashboard!')
+st.text('''As informações para o dashboard foram coletadas de sites
+na área da computação, incluindo as tecnologias necessárias e os níveis 
+de proficiência exigidos para os trabalhos. A dashboard fornecerá uma visão 
+resumida e compreensível desses requisitos, auxiliando na análise e tomada de 
+decisões relacionadas a oportunidades de emprego na área de desenvolvimento.''')
+
+st.sidebar.text('Tecnologias')set
+st.markdown('Mais informação')
+# interactive = st.beta_container()
+
+
+# Criação do widget de upload de arquivo
+# arquivo=st.file_uploader('/data_all_pages.csv', type='csv')
+
+# Leitura do arquivo CSV e armazenamento em um DataFrame
+df = pd.read_csv('df_salvo.csv')
+
+# Exibição dos dados em uma tabela
+st.write(df)
+
+st.sidebar.markdown('''
+---
+Created with ❤️ by Julia 
+''')
+
+# with interactive:
